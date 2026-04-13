a4ebd8388cec2ef214d0a2a6a0d894b01d7d77e3
zhuyixiang <1339193686@qq.com>
2026-04-13 16:35:48 +0800

    fix a;;

--- node/send_mail.js
+++ node/send_mail.js
@@ -1,15 +1,18 @@
 const nodemailer = require('nodemailer');
 
-
-let transporter = nodemailer.createTransport({
-    sendmail: true,
-    newline: 'unix',
-    path: '/usr/sbin/sendmail'
+// Create a transporter using SMTP
+const transporter = nodemailer.createTransport({
+    host: "smtphz.qiye.163.com",
+    port: 465,
+    secure: true, // use STARTTLS (upgrade connection to TLS after connecting)
+    auth: {
+        user: 'joshua@thesunowner.com',
+        pass: 'Zyx820317',
+    },
 });
 
-
 transporter.sendMail({
-    from: 'no_replay@zyxtest9929.com',
+    from: 'joshua@thesunowner.com',
     to: 'joshua@thesunowner.com',
     subject: 'Local Sendmail Test',
     text: 'Sent via local sendmail binary'
