803616d96fb53f19b2af28564a073c85a7fb4a75
zhuyixiang <1339193686@qq.com>
2026-04-13 12:06:11 +0800

    add all

---
+++ node/simple.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser=require("body-parser")
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/hello', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.post('/api/contact', function (req,res, next){
+    console.log(req.body)
+    console.log('ok')
+    res.send('ok!')
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
