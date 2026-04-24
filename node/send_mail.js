66054ac93a8dab264c9eb0be0f1a824da46b1c3b
zhuyixiang <1339193686@qq.com>
2026-04-24 15:15:51 +0800

    finish job

--- node/send_mail.js
+++ node/send_mail.js
@@ -1,19 +1,3 @@
 const nodemailer = require('nodemailer');
 
 // Create a transporter using SMTP
-const transporter = nodemailer.createTransport({
-    host: "smtphz.qiye.163.com",
-    port: 465,
-    secure: true, // use STARTTLS (upgrade connection to TLS after connecting)
-    auth: {
-        user: 'joshua@thesunowner.com',
-        pass: 'Zyx820317',
-    },
-});
-
-transporter.sendMail({
-    from: 'joshua@thesunowner.com',
-    to: 'joshua@thesunowner.com',
-    subject: 'Local Sendmail Test',
-    text: 'Sent via local sendmail binary'
-});
--- node/simple.js
+++ node/simple.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser=require("body-parser")
+const nodemailer = require("nodemailer");
 
 
 app.use(express.json());
@@ -12,8 +13,41 @@ app.get('/api/hello', (req, res) => {
 })
 
 
+
+
 app.post('/api/contact', function (req,res, next){
     console.log(req.body)
+    var message = "name: " + req.body.name + "\r\n";
+    message += "email: " + req.body.email + "\r\n";
+    if(req.body.phone){
+        message += "phone: " + req.body.phone + "\r\n";
+    }
+    if(req.body.phone){
+        message += "phone: " + req.body.phone + "\r\n";
+    }
+    if(req.body.company){
+        message += "company: " + req.body.company + "\r\n";
+    }
+    if(req.body.subject){
+        message += "subject: " + req.body.subject + "\r\n";
+    }
+    message += "content: " + req.body.content + "\r\n";
+
+    const transporter = nodemailer.createTransport({
+        host: "smtphz.qiye.163.com",
+        port: 465,
+        secure: true, // use STARTTLS (upgrade connection to TLS after connecting)
+        auth: {
+            user: 'joshua@thesunowner.com',
+            pass: 'Zyx820317',
+        },
+    });
+    transporter.sendMail({
+        from: 'joshua@thesunowner.com',
+        to: 'joshua@thesunowner.com',
+        subject: 'From Sun Owner Web',
+        text: message
+    });
     console.log('ok')
     res.send('{}')
 });
